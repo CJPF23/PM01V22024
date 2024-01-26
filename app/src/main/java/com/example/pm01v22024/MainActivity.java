cb0610dc814accade0fcd97de8293202d0dcb22e
CJPF23 <carlosj.perezf.23@gmail.com>
2024-01-25 19:45:15 -0600

    cambios realizados 25/01/2024

--- app/src/main/java/com/example/pm01v22024/MainActivity.java
+++ app/src/main/java/com/example/pm01v22024/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.pm01v22024;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nombres, apellidos, telefono;
 
-    Button btnprocesos;
+    Button btnproceso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
         nombres = (EditText) findViewById(R.id.nombres);
         apellidos = (EditText) findViewById(R.id.apellidos);
         telefono = (EditText) findViewById(R.id.telefono);
-        btnprocesos = (EditText) findViewById(R.id.btnprocesar);
+        btnproceso = (Button) findViewById(R.id.btnprocesar);
+
+        btnproceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( getApplicationContext(), nombres.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
